1d73ed0e9ab70a987db1ce117567da542d97b643
binyamyohannes <olangobinyam@gmail.com>
2024-03-27 18:33:08 +0300

    Added projects

--- src/components/projects/allProjects.jsx
+++ src/components/projects/allProjects.jsx
@@ -1,24 +1,39 @@
 import React from 'react';
 
-import Project from './project';
-
 import INFO from '../../data/user';
 
 import './styles/allProjects.css';
 
+import popupWindow from './popup';
+
+const portfolioCard = document.getElementById('portfolio_card');
+
 const AllProjects = () => (
   <div className="all-projects-container">
-    {INFO.projects.map((project) => (
-      <div className="all-projects-project" key={project.title}>
-        <Project
-          logo={project.logo}
-          title={project.title}
-          description={project.description}
-          linkText={project.linkText}
-          link={project.link}
-        />
+    {INFO.projects.forEach((project, index) => {
+      const card = document.createElement('div');
+      card.classList.add('col');
+      card.innerHTML = `
+    <div class="card" data-card-id="${index}">
+      <img src="${project.img}" class="card-img-top" alt="project_img">
+      <div class="card-body">
+        <h5 class="card-title">${project.name}</h5>
+        <div class="card-text">
+          ${project.stacks.map((stack) => (
+        `<div class="tag">${stack}</div>`
+      )).join('')}
+        </div>
+        <div class="project_btn_cont">
+          <button class="project_btn">See project</button>
+        </div>
       </div>
-    ))}
+    </div>
+  `;
+      const projectBtn = card.querySelector('.project_btn');
+      projectBtn.addEventListener('click', popupWindow);
+      portfolioCard.appendChild(card);
+    })}
+    ;
   </div>
 );
 
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -40,24 +40,12 @@ const Projects = () => {
           </div>
           <div className="projects-container">
             <div className="title projects-title">
-              Things I&apos;ve made trying to put my dent in the
-              universe.
-            </div>
-
-            <div className="subtitle projects-subtitle">
-              I&apos;ve worked on a variety of projects over the years
-              and I&apos;m proud of the progress I&apos;ve made. Many of
-              these projects are open-source and available for
-              others to explore and contribute to. If you&apos;re
-              interested in any of the projects I&apos;ve worked on,
-              please feel free to check out the code and suggest
-              any improvements or enhancements you might have in
-              mind. Collaborating with others is a great way to
-              learn and grow, and I&apos;m always open to new ideas and
-              feedback.
+              My Recent Works
             </div>
 
             <div className="projects-list">
+              <div className="row row-cols-1 row-cols-md-3 g-4 cards_cont" id="portfolio_card" />
+              <div id="popup" className="hidden" />
               <AllProjects />
             </div>
           </div>
