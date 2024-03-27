be659e532b049eede61f25582a6ff7d5e36d12aa
binyamyohannes <olangobinyam@gmail.com>
2024-03-27 17:54:43 +0300

    Update about page

--- src/components/about/socials.jsx
+++ src/components/about/socials.jsx
@@ -15,77 +15,79 @@ import INFO from '../../data/user';
 import './styles/socials.css';
 
 const Socials = () => (
-  <div className="socials">
-    <div className="social">
-      <a href={INFO.socials.twitter} target="_blank" rel="noreferrer">
-        <div className="social-icon">
-          <FontAwesomeIcon
-            icon={faTwitter}
-            className="social-icon"
-          />
-        </div>
-        <div className="social-text">Follow on Twitter</div>
-      </a>
-    </div>
+  <div className="socials social__medias__cont">
+    <div className="social__medias">
+      <div className="social">
+        <a href={INFO.socials.twitter} target="_blank" rel="noreferrer">
+          <div className="social-icon">
+            <FontAwesomeIcon
+              icon={faTwitter}
+              className="social-icon"
+            />
+          </div>
+          <div className="social-text">Follow on Twitter</div>
+        </a>
+      </div>
 
-    <div className="social">
-      <a href={INFO.socials.github} target="_blank" rel="noreferrer">
-        <div className="social-icon">
-          <FontAwesomeIcon
-            icon={faGithub}
-            className="social-icon"
-          />
-        </div>
-        <div className="social-text">Follow on GitHub</div>
-      </a>
-    </div>
+      <div className="social">
+        <a href={INFO.socials.github} target="_blank" rel="noreferrer">
+          <div className="social-icon">
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="social-icon"
+            />
+          </div>
+          <div className="social-text">Follow on GitHub</div>
+        </a>
+      </div>
 
-    <div className="social">
-      <a
-        href={INFO.socials.linkedin}
-        target="_blank"
-        rel="noreferrer"
-      >
-        <div className="social-icon">
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            className="social-icon"
-          />
-        </div>
-        <div className="social-text">Follow on LinkedIn</div>
-      </a>
-    </div>
+      <div className="social">
+        <a
+          href={INFO.socials.linkedin}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="social-icon">
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="social-icon"
+            />
+          </div>
+          <div className="social-text">Follow on LinkedIn</div>
+        </a>
+      </div>
 
-    <div className="social">
-      <a
-        href={INFO.socials.instagram}
-        target="_blank"
-        rel="noreferrer"
-      >
-        <div className="social-icon">
-          <FontAwesomeIcon
-            icon={faInstagram}
-            className="social-icon"
-          />
-        </div>
-        <div className="social-text">Follow on Instagram</div>
-      </a>
-    </div>
+      <div className="social">
+        <a
+          href={INFO.socials.instagram}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="social-icon">
+            <FontAwesomeIcon
+              icon={faInstagram}
+              className="social-icon"
+            />
+          </div>
+          <div className="social-text">Follow on Instagram</div>
+        </a>
+      </div>
 
-    <div className="social">
-      <a
-        href={INFO.socials.medium}
-        target="_blank"
-        rel="noreferrer"
-      >
-        <div className="social-icon">
-          <FontAwesomeIcon
-            icon={faMedium}
-            className="social-icon"
-          />
-        </div>
-        <div className="social-text">Follow on Medium</div>
-      </a>
+      <div className="social">
+        <a
+          href={INFO.socials.medium}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="social-icon">
+            <FontAwesomeIcon
+              icon={faMedium}
+              className="social-icon"
+            />
+          </div>
+          <div className="social-text">Follow on Medium</div>
+        </a>
+      </div>
     </div>
 
     <div className="email">
