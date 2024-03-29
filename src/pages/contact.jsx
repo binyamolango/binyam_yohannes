7067c3600c55639cbfcfeccc081945d12fe6a794
binyamyohannes <olangobinyam@gmail.com>
2024-03-29 11:06:03 +0300

    Update navbar logo

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -5,7 +5,6 @@ import { Helmet } from 'react-helmet';
 
 import NavBar from '../components/common/navBar';
 import Footer from '../components/common/footer';
-import Logo from '../components/common/logo';
 import Socials from '../components/about/socials';
 
 import INFO from '../data/user';
@@ -87,12 +86,6 @@ const Contact = () => {
       <div className="page-content">
         <NavBar active="contact" />
         <div className="content-wrapper">
-          <div className="contact-logo-container">
-            <div className="contact-logo">
-              <Logo width={46} link />
-            </div>
-          </div>
-
           <div className="contact-container">
             <div className="title contact-title">
               Let&apos;s Get in Touch
